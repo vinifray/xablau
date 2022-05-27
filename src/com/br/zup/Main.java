855dc4fd314b3e25efd772b905573ed265a8712a
Vinicius <vinicius.raymundo@zup.com.br>
2022-05-27 15:40:04 -0300

    Adicinar parte 5 da musica turu turu

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -3,13 +3,14 @@ package com.br.zup;
 public class Main {
 
     public static void main(String[] args) {
-        String parte1, parte2, parte3, parte4, parte6, parte7;
+        String parte1, parte2, parte3, parte4, parte5, parte6, parte7;
 
         parte1 = "Esse turu, turu, turu aqui dentro";
         parte2 = "Que faz turu, turu, quando passar";
         parte3 = "Meu olhar decora cada movimento";
         parte4 = "Até seu sorriso me deixa sem graça";
 
+        parte5 = "Se eu pudesse te prender";
         parte6 = "Dominar seus sentimentos";
         parte7 = "Controlar seus passos";
 
