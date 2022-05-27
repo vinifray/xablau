0e3914b7b1b3dfe3398b4c175592e9b3f35cb032
Vinicius <vinicius.raymundo@zup.com.br>
2022-05-27 15:04:31 -0300

    Adicionar parte 2 da musica turu turu

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -6,7 +6,10 @@ public class Main {
         String parte1, parte2, parte3;
 
         parte1 = "Esse turu, turu, turu aqui dentro";
+        parte2 = "Que faz turu, turu, quando passar";
+        parte3 = "Meu olhar decora cada movimento";
 
         System.out.println(parte1+" "+parte2+" "+parte3);
     }
+
 }
