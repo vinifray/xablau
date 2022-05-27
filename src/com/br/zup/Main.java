18eef7c99d97290b27e0977a5b7b262e46ba2845
Vinicius <vinicius.raymundo@zup.com.br>
2022-05-27 15:43:55 -0300

    Adicionar parte 7

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -3,13 +3,14 @@ package com.br.zup;
 public class Main {
 
     public static void main(String[] args) {
-        String parte1, parte2, parte3, parte6;
+        String parte1, parte2, parte3, parte6, parte7;
 
         parte1 = "Esse turu, turu, turu aqui dentro";
         parte2 = "Que faz turu, turu, quando passar";
         parte3 = "Meu olhar decora cada movimento";
 
         parte6 = "Dominar seus sentimentos";
+        parte7 = "Controlar seus passos";
 
         System.out.println(parte1+" "+parte2+" "+parte3);
     }
