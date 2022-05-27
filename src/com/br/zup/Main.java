e71d0b56e0c8060cc641c454ace168d80ccb3281
Vinicius <vinicius.raymundo@zup.com.br>
2022-05-27 15:03:55 -0300

    Adinar parte 1 da musica turu turu

---
+++ src/com/br/zup/Main.java
@@ -0,0 +1,12 @@
+package com.br.zup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String parte1, parte2, parte3;
+
+        parte1 = "Esse turu, turu, turu aqui dentro";
+
+        System.out.println(parte1+" "+parte2+" "+parte3);
+    }
+}
